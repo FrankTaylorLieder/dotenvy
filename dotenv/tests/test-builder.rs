2a3de2079a0f05ea723a1fa32a4f1d0c39d9c89d
Frank Taylor <frankt@vmware.com>
2024-04-04 16:20:39 +0100

    Completed tests

--- dotenv/tests/test-builder.rs
+++ dotenv/tests/test-builder.rs
@@ -8,7 +8,7 @@ use dotenvy::*;
 use crate::common::*;
 
 type InjectLoad = fn(&PathBuf) -> dotenvy::Result<Option<PathBuf>>;
-type InjectIter = fn(&PathBuf) -> dotenvy::Result<Option<Iter<File>>>;
+type InjectIter<S> = fn(&PathBuf) -> dotenvy::Result<Option<Iter<S>>>;
 
 fn check_missing_fails_load(loader: InjectLoad) -> Result<(), Box<dyn Error>> {
     println!("check_missing_fails_load");
@@ -83,23 +83,7 @@ fn check_override_load(loader: InjectLoad) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn check_builder_cannot_be_reused(loader: InjectLoad) -> Result<(), Box<dyn Error>> {
-    println!("check_builder_cannot_be_reused");
-    let _ce = CleanEnv::new();
-    let dir = make_test_dotenv()?;
-
-    let mut path = env::current_dir()?;
-    path.push(".env");
-
-    assert!(loader(&path).is_err());
-
-    env::set_current_dir(dir.path().parent().unwrap())?;
-    dir.close()?;
-
-    Ok(())
-}
-
-fn check_missing_fails_iter(loader: InjectIter) -> Result<(), Box<dyn Error>> {
+fn check_missing_fails_iter<S>(loader: InjectIter<S>) -> Result<(), Box<dyn Error>> {
     println!("check_missing_fails_iter");
     let _ce = CleanEnv::new();
     let dir = tempdir_without_dotenv()?;
@@ -114,7 +98,9 @@ fn check_missing_fails_iter(loader: InjectIter) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn check_missing_optional_iter(loader: InjectIter) -> Result<(), Box<dyn Error>> {
+fn check_missing_optional_iter<S: std::io::Read>(
+    loader: InjectIter<S>,
+) -> Result<(), Box<dyn Error>> {
     println!("check_missing_optional_iter");
     let _ce = CleanEnv::new();
     let dir = tempdir_without_dotenv()?;
@@ -138,7 +124,7 @@ fn check_missing_optional_iter(loader: InjectIter) -> Result<(), Box<dyn Error>>
     Ok(())
 }
 
-fn check_normal_iter(loader: InjectIter) -> Result<(), Box<dyn Error>> {
+fn check_normal_iter<S: std::io::Read>(loader: InjectIter<S>) -> Result<(), Box<dyn Error>> {
     println!("check_normal_iter");
     let _ce = CleanEnv::new();
     let dir = make_test_dotenv()?;
@@ -164,15 +150,23 @@ fn check_normal_iter(loader: InjectIter) -> Result<(), Box<dyn Error>> {
 
 #[test]
 #[serial]
-fn test_load_builder_default() -> Result<(), Box<dyn Error>> {
+fn test_builder_load_default() -> Result<(), Box<dyn Error>> {
     check_missing_fails_load(|_| builder::dotenv().load())?;
     check_missing_optional_load(|_| builder::dotenv().allow_missing().load())?;
     check_normal_load(|_| builder::dotenv().load())?;
     check_override_load(|_| builder::dotenv().override_duplicates().load())?;
+
+    // Additional checks to see if all options work, in any order.
     check_override_load(|_| {
         builder::dotenv()
             .allow_missing()
             .override_duplicates()
+            .load()
+    })?;
+    check_override_load(|_| {
+        builder::dotenv()
+            .override_duplicates()
+            .allow_missing()
             .load()
     })?;
 
@@ -181,7 +175,7 @@ fn test_load_builder_default() -> Result<(), Box<dyn Error>> {
 
 #[test]
 #[serial]
-fn test_load_builder_filename() -> Result<(), Box<dyn Error>> {
+fn test_builder_load_filename() -> Result<(), Box<dyn Error>> {
     check_missing_fails_load(|_| builder::from_filename(".env").load())?;
     check_missing_optional_load(|_| builder::from_filename(".env").allow_missing().load())?;
     check_normal_load(|_| builder::from_filename(".env").load())?;
@@ -192,7 +186,7 @@ fn test_load_builder_filename() -> Result<(), Box<dyn Error>> {
 
 #[test]
 #[serial]
-fn test_load_builder_path() -> Result<(), Box<dyn Error>> {
+fn test_builder_load_path() -> Result<(), Box<dyn Error>> {
     check_missing_fails_load(|p| builder::from_path(p).load())?;
     check_missing_optional_load(|p| builder::from_path(p).allow_missing().load())?;
     check_normal_load(|p| builder::from_path(p).load())?;
@@ -203,7 +197,7 @@ fn test_load_builder_path() -> Result<(), Box<dyn Error>> {
 
 #[test]
 #[serial]
-fn test_load_builder_read() -> Result<(), Box<dyn Error>> {
+fn test_builder_load_read() -> Result<(), Box<dyn Error>> {
     check_normal_load(|p| {
         builder::from_read(&mut File::open(p).expect("Provided path is missing")).load()
     })?;
@@ -218,7 +212,7 @@ fn test_load_builder_read() -> Result<(), Box<dyn Error>> {
 
 #[test]
 #[serial]
-fn test_iter_builder_default() -> Result<(), Box<dyn Error>> {
+fn test_builder_iter_default() -> Result<(), Box<dyn Error>> {
     check_missing_fails_iter(|_| builder::dotenv().iter())?;
     check_missing_optional_iter(|_| builder::dotenv().allow_missing().iter())?;
     check_normal_iter(|_| builder::dotenv().iter())?;
@@ -229,7 +223,7 @@ fn test_iter_builder_default() -> Result<(), Box<dyn Error>> {
 
 #[test]
 #[serial]
-fn test_iter_builder_filename() -> Result<(), Box<dyn Error>> {
+fn test_builder_iter_filename() -> Result<(), Box<dyn Error>> {
     check_missing_fails_iter(|_| builder::from_filename(".env").iter())?;
     check_missing_optional_iter(|_| builder::from_filename(".env").allow_missing().iter())?;
     check_normal_iter(|_| builder::from_filename(".env").iter())?;
@@ -240,11 +234,44 @@ fn test_iter_builder_filename() -> Result<(), Box<dyn Error>> {
 
 #[test]
 #[serial]
-fn test_iter_builder_path() -> Result<(), Box<dyn Error>> {
+fn test_builder_iter_path() -> Result<(), Box<dyn Error>> {
     check_missing_fails_iter(|p| builder::from_path(p).iter())?;
     check_missing_optional_iter(|p| builder::from_path(p).allow_missing().iter())?;
     check_normal_iter(|p| builder::from_path(p).iter())?;
     // Note: There is no override test as this is a function of the loader.
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_builder_iter_read() -> Result<(), Box<dyn Error>> {
+    // Note: This needs to be inlined as the loader needs to open a file and iter() returns
+    // something that references the opened file. This file will be closed when the loader closure
+    // finishes, so Rust complains the file does not live long engouh.
+    //
+    // Doing this in the same function removes the scoping issues.
+
+    println!("check_normal_iter_read");
+    let _ce = CleanEnv::new();
+    let dir = make_test_dotenv()?;
+
+    let mut path = env::current_dir()?;
+    path.push(".env");
+
+    let mut file = File::open(&path).expect("Provided path is missing");
+    let iter = builder::from_read(&mut file).iter()?;
+
+    assert!(env::var("TESTKEY").is_err());
+
+    if let Some(iter) = iter {
+        iter.load()?;
+    }
+    assert_eq!(env::var("TESTKEY")?, "test_val");
+    assert_eq!(env::var("EXISTING")?, "from_env");
+
+    env::set_current_dir(dir.path().parent().unwrap())?;
+    dir.close()?;
 
     Ok(())
 }
